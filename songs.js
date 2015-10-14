fc5096f4e758b646e5e93d540a30dfd999bd3f1c
threepears <michael@threepears.com>
2015-10-14 14:08:49 -0500

    Edit html, css, and js for version3 assignment

--- songs.js
+++ songs.js
@@ -4,7 +4,7 @@ songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[songs.length] = "Ironi!c > by Alanis Moriss*ette on the album Jagged Little Pill";
 
 //Add one song to the beginning of the array and one to the end
 songs.unshift("Brian Wilson > by Barenaked Ladies on the album Gordon");
@@ -24,7 +24,7 @@ for (var i = 0; i < songs.length; i++) {
 	var newsong = song.replace(/[*@!()]/g, "").replace(">", "-");
 	songs[i] = newsong;
 
-	var newentry = "<div>" + songs[i] + "</div><br/>";
+	var newentry = "<div>" + songs[i] + "</div>";
 	console.log(newentry);
 	output.innerHTML = output.innerHTML + newentry;
 	console.log(output.innerHTML);
